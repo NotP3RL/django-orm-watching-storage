afeca019e7b7eab7b5eecb26329f1e1635842538
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2022-08-31 18:29:23 +0800

    Сократил код

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,4 @@
-from datacenter.models import Visit
+from datacenter.models import Visit, format_duration
 from django.shortcuts import render
 from django.utils import timezone
 
@@ -11,13 +11,12 @@ def storage_information_view(request):
         time_now = timezone.now()
         delta = time_now - entered_at
         seconds_inside = delta.total_seconds()
-        hours_inside = int(seconds_inside // 3600)
-        minutes_inside = int((seconds_inside % 3600) // 60)
+        duration = format_duration(seconds_inside)
         who_entered = non_closed_visit.passcard.owner_name
         format_non_closed_visits.append({
             'who_entered': who_entered,
             'entered_at': entered_at,
-            'duration': f'{hours_inside}:{minutes_inside}'
+            'duration': duration
         })
 
     context = {

31f9f9e0415d30cdba1b0b21c8777a8212d88985
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2022-08-31 18:10:45 +0800

    Убрал комментарии из кода

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -21,6 +21,6 @@ def storage_information_view(request):
         })
 
     context = {
-        'non_closed_visits': format_non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': format_non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
